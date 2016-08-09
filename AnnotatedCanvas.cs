4a7905afc0fef124f87668e2fb5f7c6577258968
Ryan Hartley <rhartley@journaltech.com>
2016-08-09 14:48:55 -0600

    Working example with v17.5

---
+++ AnnotatedCanvas.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Leadtools.Windows.Annotations;
+
+namespace Leadtools_Issue
+{
+	public class AnnotatedCanvas : Canvas
+	{
+		private AnnContainer _container;
+		private SolidColorBrush _brush;
+		private AnnRectangleDrawDesigner _rectangleDesigner;
+
+		public void Annotate(SolidColorBrush brush)
+		{
+			_brush = brush;
+			if (_container != null) return;
+			_container = new AnnContainer
+			{
+				Width = ActualWidth,
+				Height = ActualHeight
+			};
+			RenderOptions.SetBitmapScalingMode(_container, BitmapScalingMode.HighQuality);
+			Children.Add(_container);
+		}
+
+		protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+		{
+			if (_rectangleDesigner == null && !e.Handled)
+			{
+				if (CaptureMouse())
+				{
+					var rectangleObject = new AnnRectangleObject
+					{
+						Fill = _brush.Color,
+						StrokeThickness = 0d
+					};
+					_rectangleDesigner = new AnnRectangleDrawDesigner(_container)
+					{
+						ObjectTemplate = rectangleObject
+					};
+					_rectangleDesigner.Container.ClipToBounds = true;
+					_rectangleDesigner.OnMouseLeftButtonDown(_container, e);
+				}
+			}
+			base.OnMouseDown(e);
+		}
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			SetMouseCursor(e);
+			if (_rectangleDesigner != null && e.LeftButton == MouseButtonState.Pressed && !e.Handled && _rectangleDesigner != null && IsMouseCaptured)
+			{
+				_rectangleDesigner.OnMouseMove(_rectangleDesigner.Container, e);
+			}
+			base.OnMouseMove(e);
+		}
+
+		protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+			if (_rectangleDesigner != null && !e.Handled && _rectangleDesigner != null && IsMouseCaptured)
+			{
+				ReleaseMouseCapture();
+
+				((AnnRectangleObject)_rectangleDesigner.DrawObject).Rect = GetIntersectedRectangle(_rectangleDesigner.DrawObject, _rectangleDesigner.Container);
+
+				_rectangleDesigner.OnMouseLeftButtonUp(_rectangleDesigner.Container, e);
+				_rectangleDesigner = null;
+			}
+			base.OnMouseLeftButtonUp(e);
+		}
+
+		private Rect GetIntersectedRectangle(AnnObject annObject, AnnContainer annContainer)
+		{
+			return Rect.Intersect(annObject.BoundingRectangle, new Rect(0, 0, annContainer.ActualWidth, annContainer.ActualHeight));
+		}
+
+		private void SetMouseCursor(MouseEventArgs e)
+		{
+			Cursor = Cursors.Arrow;
+			if (_container != null)
+			{
+				Cursor = Cursors.Cross;
+			}
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Leadtools;
+using Leadtools.Codecs;
+using Leadtools.Windows.Media;
+using Microsoft.Win32;
+
+namespace Leadtools_Issue
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			using (var licenseStream = typeof(MainWindow).Assembly.GetManifestResourceStream("Leadtools_Issue.License.LEADTOOLS.LIC"))
+			using (var licenseKeyStream = typeof(MainWindow).Assembly.GetManifestResourceStream("Leadtools_Issue.License.LEADTOOLS.LIC.KEY"))
+			{
+				if (licenseStream != null && licenseKeyStream != null)
+				{
+					RasterSupport.SetLicense(licenseStream, new StreamReader(licenseKeyStream).ReadToEnd());
+				}
+				else
+				{
+					throw new ApplicationException("LeadTools license not found");
+				}
+			}
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, EventArgs e)
+		{
+			string filename = null;
+			OpenFileDialog fileDialog = new OpenFileDialog { Title = "Select image or pdf to work with" };
+			bool? result = fileDialog.ShowDialog(this);
+			if (result == true)
+			{
+				filename = fileDialog.FileName;
+			}
+			RasterImage image = LoadImage(filename);
+			ImageSource rimc = RasterImageConverter.ConvertToSource(image, ConvertToSourceOptions.AutoDetectAlpha | ConvertToSourceOptions.KeepAlphaValues);
+			TransformedBitmap transformedBitmap = rimc as TransformedBitmap;
+			transformedBitmap?.Source.Freeze();
+			rimc.Freeze();
+			mainImg.Source = rimc;
+			AnnotatedCanvas.Height = rimc.Height;
+			AnnotatedCanvas.Width = rimc.Width;
+		}
+
+		private static RasterImage LoadImage(string filename)
+		{
+			if (string.IsNullOrWhiteSpace(filename)) return null;
+			using (var codecs = new RasterCodecs())
+			{
+				ConfigureCodecs(codecs, filename);
+				return codecs.Load(filename);
+			}
+		}
+
+		private static void ConfigureCodecs(RasterCodecs codecs, string filename)
+		{
+			codecs.Options.Save.RetrieveDataFromImage = true;
+			codecs.Options.Jpeg.Save.QualityFactor = 10;
+			codecs.Options.Png.Save.QualityFactor = 9;
+			codecs.Options.Pdf.Save.SavePdfv14 = true;
+			codecs.Options.Pdf.Save.SavePdfv15 = false;
+			codecs.Options.Pdf.Save.SavePdfv16 = false;
+			codecs.Options.Pdf.Save.ExtractText = true;
+			codecs.Options.Pdf.Save.ExtractTextGraphics = true;
+			codecs.Options.Pdf.Save.UseImageResolution = true;
+			codecs.Options.Pdf.Save.PrintDocument = true;
+			codecs.Options.Pdf.Save.PrintFaithful = true;
+			codecs.Options.Pdf.Save.AssembleDocument = true;
+			codecs.Options.Load.Rotated = true;
+			codecs.Options.Load.Compressed = true;
+			codecs.Options.Load.LoadCorrupted = true;
+			using (var info = codecs.GetInformation(filename, false))
+			{
+				if (info.HasResolution)
+				{
+					codecs.Options.Load.XResolution = info.XResolution;
+					codecs.Options.Load.YResolution = info.YResolution;
+				}
+				else
+				{
+					codecs.Options.Load.XResolution = 96;
+					codecs.Options.Load.YResolution = 96;
+				}
+
+				if (info.Format == RasterImageFormat.RasPdf)
+				{
+					codecs.Options.Pdf.Load.DisplayDepth = info.BitsPerPixel;
+					codecs.Options.Load.DiskMemory = true;
+					int xRes, yRes;
+					var rasInfo = codecs.GetRasterPdfInfo(filename, 1);
+					if (info.TotalPages > 1)
+					{
+						var rasInfo2 = codecs.GetRasterPdfInfo(filename, 2);
+						if (rasInfo2.Width != rasInfo.Width) rasInfo.Width = -1;
+						if (rasInfo2.Height != rasInfo.Height) rasInfo.Height = -1;
+					}
+					GetRasterResolution(info.Width, info.Height, info.XResolution, info.YResolution, rasInfo, out xRes, out yRes);
+					codecs.Options.Load.XResolution = xRes;
+					codecs.Options.Load.YResolution = yRes;
+					codecs.Options.RasterizeDocument.Load.XResolution = xRes;
+					codecs.Options.RasterizeDocument.Load.YResolution = yRes;
+				}
+
+				if (codecs.Options.Pdf.IsEngineInstalled)
+				{
+					codecs.Options.Pdf.Load.UseLibFonts = true;
+				}
+				codecs.Options.Pdf.Load.GraphicsAlpha = 1;
+				codecs.Options.Pdf.Load.TextAlpha = 1;
+			}
+		}
+
+		private static void GetRasterResolution(int imgWidth, int imgHeight, int imgXRes, int imgYRes, CodecsRasterPdfInfo rasInfo, out int xRes, out int yRes)
+		{
+			var fillsPageX = rasInfo.XResolution > 0 && rasInfo.Width > 0
+							 && rasInfo.Width <= imgWidth * rasInfo.XResolution / imgXRes;
+			var fillsPageY = rasInfo.YResolution > 0 && rasInfo.Height > 0
+							 && rasInfo.Height <= imgHeight * rasInfo.YResolution / imgYRes;
+
+			if (rasInfo.IsLeadPdf || (fillsPageX && fillsPageY))
+			{
+				xRes = rasInfo.XResolution; yRes = rasInfo.YResolution;
+			}
+			else
+			{
+				xRes = 0; yRes = 0;
+			}
+
+			int maxDimension = imgHeight;
+			int res = imgYRes;
+			if (imgWidth > imgHeight)
+			{
+				maxDimension = imgWidth;
+				res = imgXRes;
+			}
+
+			int computedResolution = Math.Min(300, 5000 / (maxDimension / res));
+			xRes = Math.Max(xRes, computedResolution);
+			yRes = Math.Max(yRes, computedResolution);
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Color color = new Color
+			{
+				A = 255,
+				R = 0,
+				G = 0,
+				B = 255	
+			};
+			AnnotatedCanvas.Annotate(new SolidColorBrush(color));
+		}
+	}
+}
